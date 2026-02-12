a38f22c6fe45003bb2530e7ed3a475829230ce4b
Artem Zhukov <64159790+atzhukov@users.noreply.github.com>
2026-02-12 22:48:01 +0100

    add restrictive policies

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -22,5 +22,6 @@ INSERT INTO tags (id, name, tenant) VALUES (1008, 'Draft', null);
 INSERT INTO tags_2_documents (id, tag, document) VALUES (10001, 1001, 11);
 INSERT INTO tags_2_documents (id, tag, document) VALUES (10002, 1002, 11);
 INSERT INTO tags_2_documents (id, tag, document) VALUES (10003, 1008, 11);
-INSERT INTO tags_2_documents (id, tag, document) VALUES (10004, 1008, 13);
-INSERT INTO tags_2_documents (id, tag, document) VALUES (10005, 1004, 12);
+INSERT INTO tags_2_documents (id, tag, document) VALUES (10004, 1004, 12);
+INSERT INTO tags_2_documents (id, tag, document) VALUES (10005, 1004, 13);
+INSERT INTO tags_2_documents (id, tag, document) VALUES (10006, 1008, 13);
